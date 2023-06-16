2c1fb5e803891f3b7bcce4c7c8f8f74505a21dee
jason-coron <jason.coron@actionnetwork.com>
2023-06-16 11:03:55 -0400

    Strip json for keys

--- driver.py
+++ driver.py
@@ -1,6 +1,8 @@
-from util import json_util
+from util import json_util, file_util
 from resources import paths
 
 if __name__ == '__main__':
-    data = json_util.get_load(paths.PATH_DK_FUTURES)
-    print(data)
+    data = json_util.serialize(paths.PATH_DK_FUTURES)
+    striped_json = json_util.strip_by_keys(data, ["name"])
+
+    file_util.write_to_file(json_util.deserliaze(striped_json), paths.PATH_JSON_TEST_OUTPUT)
